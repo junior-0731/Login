e190fc7c815821a59594ebc25f852af8c4ec1474
Edier <guerraedier197@gmail.com>
2024-10-12 19:24:52 -0500

    Se agregaron del analisis y el caso de estudio ademas se agrego el arvhio de las classes y el main

---
+++ src/all-classes.py
@@ -0,0 +1,13 @@
+#Todas las clases principales
+class veterinario:
+    def __init__(self) -> None:
+        pass
+class propietario:
+    def __init__(self) -> None:
+        pass
+class mascota:
+    def __init__(self) -> None:
+        pass
+class superUsuario:
+    def __init__(self) -> None:
+        pass
