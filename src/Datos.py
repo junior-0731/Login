c40977443a2c2866eb38dca63b186cb0cdedfa59
Junior Herrera <herrerajunior715@gnmail.com>
2024-10-14 12:06:13 -0500

    Se modifico el modulo datos (se le agregaron mas listas), agrego la clase Administrador, Y una carpeta llamada Actualizar_datos_actores la cual contendra 3 modulos que se encargaran de actualizar los datos de (Veterinario, Mascota, Propietario)

--- src/Datos.py
+++ src/Datos.py
@@ -1,4 +1,9 @@
 #En este modulo se guardan todas las listas con los datos simulando una base de datos
 lista_Propietarios = []
 lista_Mascotas = []
-lista_Veterinarios = []
+lista_Veterinarios = [] 
+
+#Listas que alamecenaran los ID
+ides_veterinarios=[]
+ides_mascotas=[]
+ides_propietarios=[]
---
+++ src/tarjeta_Veterinario.py
@@ -0,0 +1,2 @@
+def tarjeta(): # aca ira toda la informacion que se necesitara solicitar al veterinario para la tarjeta profesional
+    return "Tarjeta profesional"
